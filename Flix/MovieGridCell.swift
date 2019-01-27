7e0fe07effe366e20ecf14609495db746b26d648
keithatan <tan230@purdue.edu>
2019-01-27 18:45:50 -0500

    Set up Collection View

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Keith Tan on 1/27/19.
+//  Copyright © 2019 keithatan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
