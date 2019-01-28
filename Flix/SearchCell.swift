2b6bd4c30a35acd2c41cdbc668c9b3dcb735ab4d
keithatan <tan230@purdue.edu>
2019-01-27 20:10:07 -0500

    Search Functionality Added

---
+++ Flix/SearchCell.swift
@@ -0,0 +1,25 @@
+//
+//  SearchCell.swift
+//  Flix
+//
+//  Created by Keith Tan on 1/27/19.
+//  Copyright © 2019 keithatan. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Flix/SearchViewController.swift
+++ Flix/SearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     
 
@@ -21,8 +21,37 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         super.viewDidLoad()
         searchTable.dataSource = self
         searchTable.delegate = self
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let keywords = searchBar.text
+        let query = keywords?.replacingOccurrences(of: " ", with: "%20")
+        
+        
+        
+        let url = URL(string: "https://api.themoviedb.org/3/search/movie?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&query=\(query ?? "")&page=1&include_adult=true")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+                
+                self.movies = dataDictionary["results"] as! [[String:Any]]
+                
+                print(self.movies)
+                
+                self.searchTable.reloadData()
+                
+            }
+        }
+        task.resume()
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,8 +59,17 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell") as! SearchCell
+        
+        let movie = movies[indexPath.row]
+        
+        let title = movie["title"] as! String
+        
+        cell.titleLabel.text = title
+        
+        return cell
     }
+    
 
     /*
     // MARK: - Navigation
